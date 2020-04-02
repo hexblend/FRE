3287efcd5f19362952546be2cfd9bb359a6b2794
vladbbr <vladbbr1@gmail.com>
2020-04-02 02:17:24 +0100

    Database connection

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose
+	.connect(process.env.DB_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	})
+	.then(() => console.log('Database connected'))
+	.catch(err => console.log(err));
 
 const port = process.env.PORT || 5000;
 app.listen(() => console.log(`Server running on port ${port}.`));
