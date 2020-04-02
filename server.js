56c0bda2d5b76b4fcfc16164cd5fb11f20995b43
vladbbr <vladbbr1@gmail.com>
2020-04-02 01:59:34 +0100

    Basic server setup

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+app.listen(() => console.log(`Server running on port ${port}.`));
