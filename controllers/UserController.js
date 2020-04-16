3dbf8adb0540d023c8b7cb802b76288b23f48549
vladbbr <vladbbr1@gmail.com>
2020-04-16 16:42:17 +0100

    Update get requests to take query from params

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,8 +3,13 @@ const bcrypt = require('bcryptjs');
 
 const getAllUsers = async (req, res, next) => {
 	try {
-		const users = await User.find({ type: ['jobseeker', 'employer'] });
-		return res.json({ message: 'Success! All users have been queried', users });
+		const users = await User.find({
+			type: ['jobseeker', 'employer'],
+		});
+		return res.json({
+			message: 'Success! All users have been queried',
+			users,
+		});
 	} catch (err) {
 		const error = new Error('There are no users in the database');
 		error.status = 404;
@@ -15,7 +20,7 @@ const getAllUsers = async (req, res, next) => {
 
 const getUsersByType = async (req, res, next) => {
 	try {
-		const users = await User.find({ type: req.body.type });
+		const users = await User.find({ type: req.params.type });
 		if (users.length === 0) throw err;
 		res.json({
 			message: `Success! All users with the type of '${req.params.type}' have been queried.`,
@@ -23,7 +28,7 @@ const getUsersByType = async (req, res, next) => {
 		});
 	} catch (err) {
 		const error = new Error(
-			`'${req.params.type}' user type could not be found.`
+			`'${req.params.type}' user type could not be ound.`
 		);
 		error.status = 404;
 		next(error);
@@ -32,14 +37,17 @@ const getUsersByType = async (req, res, next) => {
 
 const getSingleUser = async (req, res, next) => {
 	try {
-		const user = await User.findById(req.body.id);
+		const user = await User.findOne({ _id: req.params.id });
 		console.log(user.favourites);
 		res.json({
-			message: 'Success! A user with the specified ID has been queried.',
+			message:
+				'Success! A user with the specified ID has been queried.',
 			user,
 		});
 	} catch (err) {
-		const error = new Error('A user with the specified ID could not be found');
+		const error = new Error(
+			'A user with the specified ID could not be found'
+		);
 		error.status = 404;
 		error.error = err;
 		next(error);
@@ -50,9 +58,17 @@ const createUser = async (req, res, next) => {
 	const { full_name, email, password, type } = req.body;
 	const hashedPassword = await bcrypt.hash(password, 10);
 	try {
-		const user = new User({ full_name, email, password: hashedPassword, type });
+		const user = new User({
+			full_name,
+			email,
+			password: hashedPassword,
+			type,
+		});
 		const savedUser = await user.save();
-		return res.json({ message: 'Success! User added successfully', savedUser });
+		return res.json({
+			message: 'Success! User added successfully',
+			savedUser,
+		});
 	} catch (err) {
 		const error = new Error('User could not be created.');
 		error.error = err;
@@ -125,34 +141,27 @@ const updateUser = async (req, res, next) => {
 	const { id, name, email, password, type } = req.body;
 
 	const updatedFields = {};
-	if (name) updatedFields.name = name;
-	if (email) updatedFields.email = email;
-	if (password) updatedFields.password = password;
-	if (type) updatedFields.type = type;
 
 	try {
-		const updatedUser = await User.updateOne({ _id: id }, updatedFields);
-		return res.json({
-			message: 'Success! The specified user has been updated',
-			updatedFields,
-			updatedUser,
-		});
 	} catch (err) {
-		const error = new Error('A user with the specified ID could not be found');
-		error.status = 404;
-		next(error);
+		const error = new Error('User could not be updated');
+		error.error = err;
+		return next(error);
 	}
 };
 
 const deleteUser = async (req, res, next) => {
 	try {
-		const user = await User.deleteOne({ _id: req.body.id });
+		const user = await User.deleteOne({ _id: req.params.id });
 		res.json({
-			message: 'Success! A user with the specified ID has been DELETED.',
+			message:
+				'Success! A user with the specified ID has been DELETED.',
 			user,
 		});
 	} catch (err) {
-		const error = new Error('A user with the specified ID could not be found');
+		const error = new Error(
+			'A user with the specified ID could not be found'
+		);
 		error.status = 404;
 		error.error = err;
 		next(error);
--- routes/users.js
+++ routes/users.js
@@ -5,13 +5,13 @@ const controller = require('../controllers/UserController');
 
 // Middleware: /api/users/
 router.get('/', controller.getAllUsers);
-router.get('/type', controller.getUsersByType);
-router.get('/get', controller.getSingleUser);
+router.get('/type/:type', controller.getUsersByType);
+router.get('/:id', controller.getSingleUser);
 router.post('/register', controller.createUser);
 router.patch('/update', controller.updateUser);
 router.patch('/addToFavs', controller.addToFavourites);
 router.patch('/makeInactive', controller.makeInactive);
 router.patch('/makeActive', controller.makeActive);
-router.delete('/', controller.deleteUser);
+router.delete('/:id', controller.deleteUser);
 
 module.exports = router;
