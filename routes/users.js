c7726acc14f1e418e5f0c3eaa2876cd7f6394a68
vladbbr <vladbbr1@gmail.com>
2020-04-18 10:18:07 +0100

    Added smarter routes

--- routes/users.js
+++ routes/users.js
@@ -5,13 +5,15 @@ const controller = require('../controllers/UserController');
 
 // Middleware: /api/users/
 router.get('/', controller.getAllUsers);
+router
+	.route('/:id')
+	.get(controller.getSingleUser)
+	.patch(controller.updateUser)
+	.delete(controller.deleteUser);
 router.get('/type/:type', controller.getUsersByType);
-router.get('/:id', controller.getSingleUser);
 router.post('/register', controller.createUser);
-router.patch('/update/:id', controller.updateUser);
 router.patch('/addToFavs/:id', controller.addToFavourites);
 router.patch('/makeInactive/:id', controller.makeInactive);
 router.patch('/makeActive/:id', controller.makeActive);
-router.delete('/:id', controller.deleteUser);
 
 module.exports = router;
