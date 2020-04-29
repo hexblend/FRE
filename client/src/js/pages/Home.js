045a19a153710a1b0a0a4a74c1f22a9bbe38cdfa
vladbbr <vladbbr1@gmail.com>
2020-04-29 09:18:50 +0100

    Finished Button Component

--- client/src/js/pages/Home.js
+++ client/src/js/pages/Home.js
@@ -4,11 +4,13 @@ import Form from '../components/Form';
 import Posts from '../components/Posts';
 
 import HomeHeader from '../layout/HomeHeader';
+import Button from '../components/Button';
 
 function Home() {
 	return (
 		<div className="Home">
 			<HomeHeader />
+			<Button icon="check" text="Finish editing" wide={true} />
 			{/* <div>
 				<h2>Articles</h2>
 				<List />
