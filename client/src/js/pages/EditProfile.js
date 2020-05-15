1be5b89ee060980facf4d1ce4471b71bb47d3ffa
vladbbr <vladbbr1@gmail.com>
2020-05-15 19:15:51 +0100

    Update profile page basic skeleton

--- client/src/js/pages/EditProfile.js
+++ client/src/js/pages/EditProfile.js
@@ -1,6 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { connect } from 'react-redux';
+
+import Button from '../components/elements/Button';
+import Input from '../components/elements/Input';
 
 import { updateHeaderView } from '../redux/actions/HeaderActions';
 import { updateLoading } from '../redux/actions/index';
@@ -17,12 +20,7 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 export const ConnectedEditProfile = (props) => {
-	const {
-		loading,
-		updateLoading,
-		loggedUser,
-		updateHeaderView,
-	} = props;
+	const { loading, updateLoading, loggedUser, updateHeaderView } = props;
 
 	const history = useHistory();
 
@@ -52,11 +50,69 @@ export const ConnectedEditProfile = (props) => {
 		}
 	}, [updateHeaderView, loading]);
 
+	// Modal
+	const [openModal, setOpenModal] = useState(false);
+
+	const [fullName, setFullName] = useState('');
+	const [fullNameError, setFullNameError] = useState('');
+
+	const [email, setEmail] = useState('');
+	const [emailError, setEmailError] = useState('');
+
 	return (
 		<>
 			{!loading && (
-				<div>
-					<h1>Edit profile</h1>
+				<div className="EditProfile__content">
+					<div className="EditProfile__splitView">
+						{/* Left Side */}
+						<div className="EditProfile__splitView--left">
+							<section className="EditProfile__section">
+								<h3 className="EditProfile__section--title">
+									General Information
+								</h3>
+								<Input
+									type="text"
+									id="fullName"
+									label="Full name"
+									placeholder="First and last name"
+									minWidth="100%"
+									value={fullName}
+									handleChange={setFullName}
+									error={fullNameError}
+								/>
+								<Input
+									type="email"
+									id="email"
+									label="Your email"
+									placeholder="Your email"
+									minWidth="100%"
+									value={email}
+									handleChange={setEmail}
+									error={emailError}
+								/>
+							</section>
+							<h3 className="EditProfile__sectionTitle">Badges</h3>
+							<h3 className="EditProfile__sectionTitle">Key abilities</h3>
+							<h3 className="EditProfile__sectionTitle">Experience</h3>
+						</div>
+						{/* Right Side */}
+						<div className="EditProfile__splitView--right">
+							<h3 className="EditProfile__sectionTitle">
+								Projects / Achivements / Activities
+							</h3>
+							<h3 className="EditProfile__sectionTitle">Useful links</h3>
+						</div>
+					</div>
+
+					<div className="EditProfile__buttons">
+						<div></div>
+						<Button
+							text="Delete profile"
+							btnType="button"
+							type="delete"
+							onClick={() => setOpenModal(true)}
+						/>
+					</div>
 				</div>
 			)}
 		</>
