d169bf221e63a599bef2b599208b01ca11219623
vladbbr <vladbbr1@gmail.com>
2020-05-15 19:36:49 +0100

    UpdateLoggedProfile Redux state

--- client/src/js/redux/actions/ProfileActions.js
+++ client/src/js/redux/actions/ProfileActions.js
@@ -1,5 +1,11 @@
-import { UPDATE_PROFILE } from '../constants/action-types';
+import {
+	UPDATE_PROFILE,
+	UPDATE_LOGGED_PROFILE,
+} from '../constants/action-types';
 
 export function updateProfile(payload) {
 	return { type: UPDATE_PROFILE, payload };
+}
+export function updateLoggedProfile(payload) {
+	return { type: UPDATE_LOGGED_PROFILE, payload };
 }
