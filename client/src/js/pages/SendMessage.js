f9f794f4a1bb42b7f7f17edd1080437a8efea124
vladbbr <vladbbr1@gmail.com>
2020-05-27 10:22:58 +0100

    Send message fallback if profile is not set

--- client/src/js/pages/SendMessage.js
+++ client/src/js/pages/SendMessage.js
@@ -1,10 +1,24 @@
 import React, {useEffect} from 'react';
 import {connect} from 'react-redux';
+import {useHistory} from 'react-router-dom';
 import {updateHeaderView} from '../redux/actions/HeaderActions';
+import isEmpty from '../components/isEmpty';
 
 const SendMessage = props => {
-    const {updateHeaderView, loggedUser} = props;
+    const {updateHeaderView, loggedUser, profile} = props;
+    const PUBLIC_URL = process.env.REACT_APP_PUBLIC_URL;
+    const history = useHistory();
 
+    // Go to profile if is not set
+    useEffect(() => {
+        const url = window.location.href;
+        const profileID = props.match.params.id;
+        if (url.includes('/profile/') && isEmpty(profile)) {
+            history.push(`${PUBLIC_URL}/profile/${profileID}`);
+        }
+    }, []);
+
+    // Update header title
     useEffect(() => {
         updateHeaderView('sendMessage');
     }, [updateHeaderView]);
@@ -22,6 +36,7 @@ const mapDispatchToProps = dispatch => ({
 
 const mapStateToProps = state => ({
     loggedUser: state.AuthReducer.loggedUser,
+    profile: state.ProfileReducer.profile,
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(SendMessage);
