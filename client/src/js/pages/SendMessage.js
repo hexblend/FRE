eb67a103d6c2e4847d1be13ee42908c4d01bff48
vladbbr <vladbbr1@gmail.com>
2020-05-27 11:34:55 +0100

    Get profile from url on send Message page

--- client/src/js/pages/SendMessage.js
+++ client/src/js/pages/SendMessage.js
@@ -5,25 +5,29 @@ import axios from 'axios';
 import isEmpty from '../components/isEmpty';
 import Input from '../components/elements/Input';
 import Button from '../components/elements/Button';
+import Alert from '../layout/Alert';
 
 import {connect} from 'react-redux';
 import {updateHeaderView} from '../redux/actions/HeaderActions';
+import {updateProfile} from '../redux/actions/ProfileActions';
 
 const SendMessage = props => {
-    const {updateHeaderView, loggedUser, profile} = props;
+    const {updateHeaderView, loggedUser, profile, updateProfile} = props;
     const [newMessage, setNewMessage] = useState('');
     const [messageError, setMessageError] = useState('');
+    const [alert, setAlert] = useState({type: '', text: ''});
 
     const PUBLIC_URL = process.env.REACT_APP_PUBLIC_URL;
+    const API_URL = process.env.REACT_APP_API_URL;
     const history = useHistory();
 
-    // Go to profile if is not set
+    // Update profile on load
     useEffect(() => {
-        const url = window.location.href;
         const profileID = props.match.params.id;
-        if (url.includes('/profile/') && isEmpty(profile)) {
-            history.push(`${PUBLIC_URL}/profile/${profileID}`);
-        }
+        axios
+            .get(`${API_URL}/api/users/${profileID}`, {useCredentials: true})
+            .then(res => updateProfile(res.data.user))
+            .catch(() => history.send(`${PUBLIC_URL}`));
     }, []);
 
     // Update header title
@@ -36,11 +40,29 @@ const SendMessage = props => {
             setMessageError("You can't send and empty message");
         } else {
             setMessageError('');
-            console.log(newMessage);
+
+            const newText = {
+                from: loggedUser._id,
+                message: newMessage,
+            };
+            axios
+                .post(
+                    `${API_URL}/api/users/send_message/${profile._id}`,
+                    newText,
+                    {useCredentials: true},
+                )
+                .then(() => {
+                    setAlert({type: 'success', text: 'Message sent.'});
+                    const profileID = props.match.params.id;
+                    setTimeout(() => {
+                        history.goBack();
+                    }, 1500);
+                });
         }
     };
     return (
         <>
+            <Alert type={alert.type} text={alert.text} />
             <div className="SendMessage">
                 <Input
                     id="newMessage"
@@ -62,6 +84,7 @@ const SendMessage = props => {
 
 const mapDispatchToProps = dispatch => ({
     updateHeaderView: str => dispatch(updateHeaderView(str)),
+    updateProfile: obj => dispatch(updateProfile(obj)),
 });
 
 const mapStateToProps = state => ({
