974c766ef24a4dadf01c2fac0a9741b464007d45
vladbbr <vladbbr1@gmail.com>
2020-06-21 19:29:00 +0100

    Home Image Sections Responsive

--- client/src/js/pages/Home.js
+++ client/src/js/pages/Home.js
@@ -25,25 +25,38 @@ function ConnectedHome({ loggedUser }) {
 								<Button text="Login as Candidate" />
 							</Link>
 							<Link to={`${PUBLIC_URL}/employer/login`}>
-								<Button text="Login as Employer" type="secondary" />
+								<Button
+									text="Login as Employer"
+									type="secondary"
+								/>
 							</Link>
 						</div>
 					</div>
 				)}
 
 				<div className="Home__bigCard job">
-					<img src="assets/images/handshake.jpg" alt="Handshake" />
+					<div
+						className="image-side"
+						style={{
+							backgroundImage: `url('assets/images/handshake.jpg')`,
+						}}
+					></div>
 					<div className="text-side">
 						<h3>Looking for a job?</h3>
 						<p>
-							It’s that easy. Just create a profile as Candidate, upload all your
-							information that is needed for an employer to make a good idea about you and
-							sit tight! At any moment, somebody may pick your details and get in touch
-							with you.
+							It’s that easy. Just create a profile as
+							Candidate, upload all your information that is
+							needed for an employer to make a good idea
+							about you and sit tight! At any moment,
+							somebody may pick your details and get in
+							touch with you.
 						</p>
 						{isEmpty(loggedUser) && (
 							<Link to={`${PUBLIC_URL}/candidate/register`}>
-								<Button text="Register as Candidate" type="secondary" />
+								<Button
+									text="Register as Candidate"
+									type="secondary"
+								/>
 							</Link>
 						)}
 					</div>
@@ -53,19 +66,30 @@ function ConnectedHome({ loggedUser }) {
 					<div className="text-side">
 						<h3>Looking for a staff?</h3>
 						<p>
-							If you are an employer, you have the possibility to search through a list of
-							candidates and pick the right one for your needs. It’s so simple that you
-							don’t even need an account to do all this. It’s all right here, just search
-							by job title and location. Oh, and you can even look for multiple job titles
-							at the same time.
+							If you are an employer, you have the
+							possibility to search through a list of
+							candidates and pick the right one for your
+							needs. It’s so simple that you don’t even need
+							an account to do all this. It’s all right
+							here, just search by job title and location.
+							Oh, and you can even look for multiple job
+							titles at the same time.
 						</p>
 						{isEmpty(loggedUser) && (
 							<Link to={`${PUBLIC_URL}/employer/register`}>
-								<Button text="Register as Employer" type="secondary" />
+								<Button
+									text="Register as Employer"
+									type="secondary"
+								/>
 							</Link>
 						)}
 					</div>
-					<img src="assets/images/empty_office.jpg" alt="Empty office" />
+					<div
+						className="image-side"
+						style={{
+							backgroundImage: `url('assets/images/empty_office.jpg')`,
+						}}
+					></div>
 				</div>
 			</div>
 		</div>
